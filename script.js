919d1dba3c8e17709213d74579d725ef2cfff3bb
Thomas <helloworld.pdf@gmail.com>
2020-12-21 11:48:08 +0100

    Updated calculator

--- script.js
+++ script.js
@@ -11,11 +11,13 @@ input.addEventListener("keydown", function(event) {
 //input validation
 function Input() {
  result.innerHTML = "";
+ console.clear();
+ console.log("input type is: " + typeof(input.value));
  try {
-  if (Number.isInteger(input.value)) throw "Please enter an integer";
   if (input.value < 0) throw "Please enter a positive number";
   if (input.value == "") throw "Please enter a number";
-  if (input.value == 0) throw "Please enter a number that is different from 0"; }
+  if (input.value == 0) throw "Please enter a number that is different from 0";
+  if (input.value.includes(".")) throw "Please enter an integer"; }
  catch (error) { response.innerHTML = error; return false }
  response.innerHTML = "Prime factor decomposition for " + "<a style=color:#4dc3ff;text-decoration:none>" + input.value + "</a>" + ":";
  Calculation(); }
@@ -30,6 +32,7 @@ function Calculation() {
   while (dividende % diviseur == 0) {
    resultat = dividende / diviseur;
    dividende = resultat;
-   text += "<br>" + diviseur; }
+   text += "<br>" + diviseur;
+   console.log(dividende + " / " + diviseur) }
   diviseur += 1; }
  result.innerHTML = text; }
