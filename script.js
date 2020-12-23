7fa335fdfefe2970c331761d1b05e60b5da60765
Thomas <helloworld.pdf@gmail.com>
2020-12-23 18:56:30 +0100

    Updated Calculator

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//JavaScript code (interface: default + console)
+//JavaScript code
 console.log("%cState: operative", "font-weight:bold;font-size:15px;color:green");
 
 let input = document.querySelector("input");
@@ -6,12 +6,16 @@ let response = document.querySelector("h2");
 let button = document.querySelector("button");
 let show_result = document.getElementById("result");
 let info = document.getElementById("info");
+let time_loop = 0;
+let intervalTime;
 
+//Validate Input with enter
 input.addEventListener("keydown", function(event) {
   if (event.keyCode === 13) { event.preventDefault(); button.click() }
 })
 
-function Calculation() {
+//Input Validation
+function ValidateInput() {
   show_result.innerHTML = "";
   console.clear();
   console.log("%cState: operative", "font-weight:bold;font-size:15px;color:#00ff00");
@@ -27,21 +31,27 @@ function Calculation() {
     console.log("%cInput response: " + '"' + error + '"', "font-size:15px;color:red");
     return false
   }
-
   info.style.display = "inline";
   console.info("%cPrime factor decomposition for " + input.value + ":", "font-size:15px;color:#4dc3ff");
+  function TimeLoop() { time_loop++ };
+  intervalTime = setInterval(TimeLoop, 1);
+  Calculation();
+}
 
+//Function for Prime Factorization + console interface
+function Calculation() {
   let dividend = input.value;
   let divisor = 2;
   let result;
   var text = "";
-  let count_loop = 0;
+  let count_calculation = 0;
+  let count_loop = -1;
   while (dividend > 1) {
     var exponent = 0;
     while (dividend % divisor == 0) {
       result = dividend / divisor;
-      count_loop++;
-      console.info("%cCalculation " + "(" + count_loop + ")" + ":", "color:coral; font-size:15px");
+      count_calculation++;
+      console.info("%cCalculation " + "(" + count_calculation + ")" + ":", "color:coral;font-size:15px");
       console.log("%c " + dividend + " \u00f7 " + divisor, "font-size:14px");
       dividend = result;
       exponent += 1;
@@ -50,12 +60,15 @@ function Calculation() {
       text += "<br>" + divisor + "<sup style='color:#55dda4'>" + exponent + "</sup>"
     }
     divisor += 1;
+    count_loop++;
   }
 
+  clearInterval(intervalTime);
   console.info("%cCalculation finished", "font-size:15px;color:#4dc3ff");
+  console.info("%cThe divisor (initially set to 2) increased " + count_loop + " time(s). The program went on for " + time_loop + " milliseconds before finding results.", "font-size:15px;color:#bd66ff")
 
   if (divisor - 1 == input.value) {
-    response.innerHTML = "<a style=color:#4dc3ff;text-decoration:none>" + input.value + "</a>" + " is a " + "<a style='color:white' href='https://en.wikipedia.org/wiki/Prime_number' target='_blank'>" + "prime number" + "</a>" + " !";
+    response.innerHTML = "<a style=color:#4dc3ff;text-decoration:none>" + input.value + "</a>" + " is a " + "prime number" + " !";
   } else {
     response.innerHTML = "Prime factor decomposition for " + "<a style=color:#4dc3ff;text-decoration:none>" + input.value + "</a>" + ":";
   }
