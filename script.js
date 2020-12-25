5bb479d36f295f7671e72f850d89b68022e59557
helloworld-html <helloworld.pdf@gmail.com>
2020-12-25 14:21:41 +0100

    added features

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ let response = document.querySelector("h2");
 let button = document.querySelector("button");
 let show_result = document.getElementById("result");
 let info = document.getElementById("info");
-let time_loop = 0;
-let intervalTime;
 
 //Validate Input with enter
 input.addEventListener("keydown", function(event) {
@@ -33,8 +31,7 @@ function ValidateInput() {
   }
   info.style.display = "inline";
   console.info("%cPrime factor decomposition for " + input.value + ":", "font-size:15px;color:#4dc3ff");
-  function TimeLoop() { time_loop++ };
-  intervalTime = setInterval(TimeLoop, 1);
+  console.time("The program went on for");
   Calculation();
 }
 
@@ -63,9 +60,12 @@ function Calculation() {
     count_loop++;
   }
 
-  clearInterval(intervalTime);
   console.info("%cCalculation finished", "font-size:15px;color:#4dc3ff");
-  console.info("%cThe divisor (initially set to 2) increased " + count_loop + " time(s). The program went on for " + time_loop + " milliseconds before finding results.", "font-size:15px;color:#bd66ff")
+
+  console.groupCollapsed("%cComplementary Information", "font-size:15px;color:#bd66ff;font-weight:bold");
+  console.info("The divisor (initially set to 2) increased " + count_loop + " time(s).");
+  console.timeEnd("The program went on for");
+  console.groupEnd();
 
   if (divisor - 1 == input.value) {
     response.innerHTML = "<a style=color:#4dc3ff;text-decoration:none>" + input.value + "</a>" + " is a " + "prime number" + " !";
